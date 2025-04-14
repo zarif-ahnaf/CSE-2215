5d55ffcdf970a002ef2c32d5fc707acf4cda9969
zarifahnaf <86199070+zarif-ahnaf@users.noreply.github.com>
2025-04-14 19:10:37 +0600

    Add

--- Day 5/In Lab/Test.java
+++ Day 5/In Lab/Test.java
@@ -1,13 +1,13 @@
+// Test.java 
+
 import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
         Tester tester = new Tester();
-        System.out.print("Enter a password to check its format:");
         try (Scanner scanner = new Scanner(System.in);) {
-
             while (true) {
-
+                System.out.print("Enter Password:");
                 String password = scanner.nextLine();
 
                 if (tester.checkPassword(password)) {
